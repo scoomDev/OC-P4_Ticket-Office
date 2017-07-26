78b00cf0f2a8274e1449f82d1178cdf6f09ad9c0
scoomarts <soetaert.chris@gmail.com>
2017-07-26 18:03:19 +0200

    Setting the index page. to do : - entities - forms - relations between entities

---
+++ web/js/initMaterialize.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    var day = new Date();
+    var year = day.getFullYear();
+    var month = day.getMonth();
+
+    $('.datepicker').pickadate({
+        min: day,
+        max: new Date(year+2,month,01),
+        disable: [
+            2,7,
+            new Date(year,11,25),
+            new Date((year+1),11,25),
+            new Date((year+2),11,25),
+            new Date(year,10,01),
+            new Date((year+1),10,01),
+            new Date((year+2),10,01),
+            new Date(year,04,01),
+            new Date((year+1),04,01),
+            new Date((year+2),04,01),
+        ]
+    });
+});
